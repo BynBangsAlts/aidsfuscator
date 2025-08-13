d1a2e805d5967c42ac848123514d76512518f64b
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2025-08-13 17:18:02 +0300

    if confuser (flow)

--- src/main/java/dev/lvstrng/aids/Main.java
+++ src/main/java/dev/lvstrng/aids/Main.java
@@ -1,21 +1,24 @@
 package dev.lvstrng.aids;
 
 import dev.lvstrng.aids.jar.Dependencies;
+import dev.lvstrng.aids.transform.impl.data.ArithmeticTransformer;
 import dev.lvstrng.aids.transform.impl.data.IntegerEncryptTransformer;
 import dev.lvstrng.aids.transform.impl.data.StringEncryptTransformer;
 import dev.lvstrng.aids.transform.impl.flow.FlowFlattenTransformer;
+import dev.lvstrng.aids.transform.impl.flow.IfConfuser;
 import dev.lvstrng.aids.transform.impl.flow.LightFlowTransformer;
 import dev.lvstrng.aids.transform.impl.flow.UselessExceptionTransformer;
 import org.objectweb.asm.ClassWriter;
 
 public class Main {
     public static void main(String[] args) {
-        var obfuscator = new Obfuscator("eval.jar", ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        var obfuscator = new Obfuscator("in.jar", ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
         Dependencies.analyze("dependencies/");
 
         obfuscator.readInput();
         obfuscator.obfuscate(
-                new LightFlowTransformer()
+                new FlowFlattenTransformer(),
+                new IfConfuser()
         );
         obfuscator.saveOutput("out.jar");
     }
