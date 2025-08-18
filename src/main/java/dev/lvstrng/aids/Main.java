969b9181957a2984e2467f0f946fe0dfc04dbcbf
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2025-08-18 22:18:01 +0300

    Revert "method renamer (UNFINISHED)"
    This reverts commit 7ce90d7604f3014416c025171047e1332976a803.

--- src/main/java/dev/lvstrng/aids/Main.java
+++ src/main/java/dev/lvstrng/aids/Main.java
@@ -1,18 +1,11 @@
 package dev.lvstrng.aids;
 
 import dev.lvstrng.aids.jar.dependencies.Dependencies;
-import dev.lvstrng.aids.transform.impl.data.ArithmeticTransformer;
-import dev.lvstrng.aids.transform.impl.data.IntegerEncryptTransformer;
-import dev.lvstrng.aids.transform.impl.data.StringEncryptTransformer;
-import dev.lvstrng.aids.transform.impl.flow.FlowFlattenTransformer;
-import dev.lvstrng.aids.transform.impl.flow.IfConfuser;
-import dev.lvstrng.aids.transform.impl.flow.UselessExceptionTransformer;
-import dev.lvstrng.aids.transform.impl.rename.MethodRenameTransformer;
 import org.objectweb.asm.ClassWriter;
 
 public class Main {
     public static void main(String[] args) {
-        var obfuscator = new Obfuscator("evaluator-2.jar", ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        var obfuscator = new Obfuscator("eval.jar", ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
         Dependencies.analyze("dependencies/");
 
         obfuscator.readInput();
